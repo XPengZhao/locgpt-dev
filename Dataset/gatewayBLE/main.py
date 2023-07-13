0fb4691fdc16de4c9dedcb06836db319ba61af5d
Zhenlin An <an@tagsys.org>
2023-07-13 23:19:36 +0800

    update two tags

--- Dataset/gatewayBLE/main.py
+++ Dataset/gatewayBLE/main.py
@@ -11,7 +11,7 @@ from datetime import datetime
 
 broker = 'localhost'
 port = 1883
-topic = "silabs/aoa/iq_report/ble-pd-0C4314F46BF8/ble-pd-0C4314EF65A1"
+topic = "silabs/aoa/iq_report/ble-pd-0C4314F46D2F/ble-pd-0C4314EF65A1"
 topic2 = "silabs/aoa/iq_report/ble-pd-0C4314F46D2F/ble-pd-B43A31EEB7B6"
 topic_array = [(topic, 0), (topic2, 1)]
 client_id = f'python-mqtt-{random.randint(0, 100)}'
@@ -39,8 +39,10 @@ def subscribe(client: mqtt_client, mc):
         data = json.loads(msg.payload.decode())
         if msg.topic == topic:
             data_pub = {"id":1, "frequency": data["channel"], "timestamp": datetime.now().isoformat(), "rssi": data["rssi"], "samples": data["samples"]}
+            print("tag 1")
         elif msg.topic == topic2:
             data_pub = {"id":2, "frequency": data["channel"], "timestamp": datetime.now().isoformat(), "rssi": data["rssi"], "samples": data["samples"]}
+            print("tag 2")
         mc.sendData(json.dumps(data_pub))
         print("Message received from MQTT and sent to RabbitMQ")
         # with open(filename, 'a') as f:
