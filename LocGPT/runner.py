40a009658635235285c877217d89de209687499a
xiaopeng <xpzhao@smail.nju.edu.cn>
2023-08-01 19:31:42 +0800

    2 enc

--- LocGPT/runner.py
+++ LocGPT/runner.py
@@ -432,7 +432,7 @@ class LocGPT_Runner():
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config', type=str, default='conf/pretrain/pretrain-exp2.yaml', help='config file path')
+    parser.add_argument('--config', type=str, default='conf/pretrain/pretrain-exp1.yaml', help='config file path')
     parser.add_argument('--gpu', type=int, default=0)
     parser.add_argument('--mode', type=str, default='train')
     args = parser.parse_args()
